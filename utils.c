d1c5128e84dd7b3275dc5c35d98cf3b4983fba0a
p.leroux <pleroux@student.42.fr>
2017-11-20 15:54:18 +0100

    add init_fillit & free t_fillit

--- utils.c
+++ utils.c
@@ -6,16 +6,30 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 13:39:10 by yguaye            #+#    #+#             */
-/*   Updated: 2017/11/20 13:47:57 by yguaye           ###   ########.fr       */
+/*   Updated: 2017/11/20 15:53:35 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 int		flt_puterror(char *reason)
 {
 	ft_putstr_fd("\x1b[1;31m\x1b[4mError:\x1b[0m ", STDERR_FILENO);
 	ft_putendl_fd(reason, STDERR_FILENO);
 	return (-1);
+}
+
+void	free_fillit(t_fillit *t)
+{
+	int			i;
+
+	i = 0;
+	while (i < 27)
+	{
+		free(t->ttms_tab[i]);
+		i++;
+	}
+	free(t->grid);
 }
