fe13b008f8f9646b9e0ed1f645af458f1cd86365
MisterPeModder <yguaye44@gmail.com>
2017-11-21 13:10:34 +0100

    Normification! (not an actual english word)

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguqye@student42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 11:42:04 by yguaye            #+#    #+#             */
-/*   Updated: 2017/11/21 12:19:46 by pleroux          ###   ########.fr       */
+/*   Updated: 2017/11/21 13:09:23 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,38 +22,38 @@
 # define		SET					1
 # define		MOD(x)				((x < 0) ? 0 : x)
 
-typedef			unsigned char		t_bool;
+typedef unsigned char	t_bool;
 
-typedef struct	s_ttms
+typedef struct		s_ttms
 {
 	size_t			index;
 	/*int			width;*/
 	/*int			height;*/
-	char		offset[3];
-}				t_ttms;
+	char			offset[3];
+}					t_ttms;
 
-typedef struct	s_fillit
+typedef struct		s_fillit
 {
-	t_ttms		*ttms_tab[27];
-	size_t		tab_len;
-	char		*grid;
-	size_t		grid_size;
-	size_t		grid_len;
-}				t_fillit;
+	t_ttms			*ttms_tab[27];
+	size_t			tab_len;
+	char			*grid;
+	size_t			grid_size;
+	size_t			grid_len;
+}					t_fillit;
 
-void			init_fillit(t_fillit *t);
-void			init_grid(t_fillit *t);
-void			set_tablen(t_fillit *t, int len);
-int				ft_sqrt(int nb);
-int				read_ttms(t_fillit *flt, char *path);
-int				create_ttms(char *buff, t_ttms **nt);
-int				flt_puterror(char *reason);
-void			free_fillit(t_fillit *t);
-void			set_grid_size(t_fillit *t, int len);
-void			grid_up(t_fillit *t);
-int				set_ttms(t_fillit *t, int i);
-int				search_empty_case(t_fillit *t, size_t index);
-t_bool			set_or_check_ttms(t_fillit *t, int i, t_bool b_set_or_check);
-void			remove_ttms(t_fillit *t, size_t i);
+void				init_fillit(t_fillit *t);
+void				init_grid(t_fillit *t);
+void				set_tablen(t_fillit *t, int len);
+int					ft_sqrt(int nb);
+int					read_ttms(t_fillit *flt, char *path);
+int					create_ttms(char *buff, t_ttms **nt);
+int					flt_puterror(char *reason);
+void				free_fillit(t_fillit *t);
+void				set_grid_size(t_fillit *t, int len);
+void				grid_up(t_fillit *t);
+int					set_ttms(t_fillit *t, int i);
+int					search_empty_case(t_fillit *t, size_t index);
+t_bool				set_or_check_ttms(t_fillit *t, int i, t_bool b_set_check);
+void				remove_ttms(t_fillit *t, size_t i);
 
 #endif
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 13:39:10 by yguaye            #+#    #+#             */
-/*   Updated: 2017/11/20 17:46:34 by pleroux          ###   ########.fr       */
+/*   Updated: 2017/11/21 13:06:48 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int				flt_puterror(char *reason)
 {
 	ft_putstr_fd("\x1b[1;31m\x1b[4mError:\x1b[0m ", STDERR_FILENO);
 	ft_putendl_fd(reason, STDERR_FILENO);
-	return (-1);
+	return (ERROR);
 }
 
 void			free_fillit(t_fillit *t)
