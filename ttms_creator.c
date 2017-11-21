b24e790b149da4e8ffc2caf77a822d3f115437c0
MisterPeModder <yguaye44@gmail.com>
2017-11-21 15:05:57 +0100

    ttms creator

--- ttms_creator.c
+++ ttms_creator.c
@@ -6,31 +6,51 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 17:49:01 by yguaye            #+#    #+#             */
-/*   Updated: 2017/11/21 13:32:14 by yguaye           ###   ########.fr       */
+/*   Updated: 2017/11/21 15:04:54 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdlib.h>
 
-static void		suppress_vertical_lines(t_ttms *nt)
+static t_bool	check_ttms(t_ttms *nt)
 {
-/*'Removes' blank lines to the right and the left of the ttms.*/
-}
-
-static void		suppress_horizontal_lines(t_ttms *nt)
-{
-/*'Removes' blank lines below and above the ttms.*/
+	return (0);
 }
 
 int				create_ttms(char *buff, t_ttms **nt)
 {
+	int		i;
+	int		j;
+	int		offset_index;
+
 	if (!(*nt = (t_ttms *)malloc(sizeof(t_ttms))))
 		return (flt_puterror("An error occured while creating a tetriminos."));
-	/*	-=Tttms reading system=-
-	 *	*soon!*
-	 */
-	suppress_horizontal_lines(*nt);
-	suppress_vertical_lines(*nt);
+	i = 0;
+	j = 0;
+	offset_index = -1;
+	while (i < 21)
+	{
+		if (buff[i] == '#')
+		{
+			if (offset_index > 2)
+			{
+				free(*nt);
+				return (flt_puterror("A ttms cannot have more than 4 blocks"));
+			}
+			else if (offset_index != -1)
+				(*nt)->offset[offset_index] = j;
+			++offset_index;
+			j = 0;
+		}
+		else
+			j++;
+		++i;
+	}
+	if (!check_ttms(*nt))
+	{
+		free(*nt);
+		return (ERROR);
+	}
 	return (0);
 }
